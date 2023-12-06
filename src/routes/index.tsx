2a7114fab1aa64ebe78182ea494d8bcdce3178ee
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-05 23:45:03 -0300

    att routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,14 @@ import { AuthContext } from "../contexts/AuthContext";
 import { AuthPage } from "../pages/AuthPage/AuthPage";
 import { protectedRoutes } from "./protected";
 import { publicRoutes } from "./public";
+import { InfoPage } from "../components/InfoPage/infoPage";
 
 export const AppRoutes = () => {
   const { user } = useContext(AuthContext);
 
   const commonRoutes = [
-    { path: "/", element: <AuthPage /> },
+    { path: "/", element: <InfoPage /> },
+    { path: "/auth/*", element: <AuthPage /> },
     { path: "*", element: <Navigate to="." /> },
   ];
 
