a6dd5cf09d347e0d90503ec0b2730d57fce1bae0
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-05 23:42:19 -0300

    create navBar from infoPage
    Co-authored-by: Gabriel Diaz <Igwbriel@users.noreply.github.com>

---
+++ src/components/InfoPage/infoPage.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer } from "./styles/infoPage";
+
+export function InfoPage() {
+    return (
+        <HeaderContainer>
+            <div className="left-content" >
+                <strong>
+                    FetchCompany
+                </strong>
+            </div>
+            <div className="right-content">
+                <a href="" >
+                    Sobre
+                </a>
+                <a href="">
+                    Contatos
+                </a>
+            </div>
+
+
+        </HeaderContainer>
+    )
+}
