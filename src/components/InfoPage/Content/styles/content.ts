4b7b522fb8ade3d8ac08ae04f9e89511c55dff31
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-06 20:02:13 -0300

    style for content

--- src/components/InfoPage/Content/styles/content.ts
+++ src/components/InfoPage/Content/styles/content.ts
@@ -1,29 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    background-color: var(--hub-blue); 
+
     .content-container {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    height: 100vh; 
-}
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 91.6vh;
+    }
 
-.text-container {
-    padding: 20px;
-    border: 1px solid #ccc;
-    text-align: center;
-    width: 30%; 
-}
-
-#text1 {
-    background-color: #ffd700; 
-}
-
-#text2 {
-    background-color: #87ceeb; 
-}
-
-#text3 {
-    background-color: #90ee90; 
-}
-`
+    .text-container {
+        border-radius: 8px;
+        padding: 20px;
+        border: 1px solid #ccc;
+        text-align: center;
+        width: 30%;
+    }
+`;
