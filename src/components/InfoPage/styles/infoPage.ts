6692271c28345d1ba9b09b645cad9a92d5c571f4
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-05 23:44:41 -0300

    styles for infoPage

---
+++ src/components/InfoPage/styles/infoPage.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  color: var(--hub-green);
+  background-color: var(--hub-green);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px; 
+
+  .left-content {
+    background-color: lightgray;
+    padding: 8px; 
+    border-radius: 8px;
+  }
+
+  .right-content {
+    display: flex;
+    align-items: center;
+
+    a {
+      background-color: lightgray; 
+      margin-left: 10px; 
+      text-decoration: none;
+      color: var(--hub-green);
+      padding: 5px; 
+      border-radius: 8px;
+    }
+    a:hover {
+      background: var(--hub-blue);
+      color:black; 
+      
+    }
+  }
+`;
