f9fdc0087bf40607bb1c299f0ae3a3878655a5ae
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-07 22:11:45 -0300

    improve transition

--- src/components/InfoPage/Content/styles/content.ts
+++ src/components/InfoPage/Content/styles/content.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background-color: var(--hub-green); 
+    background-color: var(--hub-green);
     overflow: hidden;
 
     .content-container {
@@ -19,14 +19,16 @@ export const Container = styled.div`
         padding: 20px;
         text-align: center;
         width: 30%;
-        margin: 10px; 
+        margin: 10px;
         font-weight: bold;
-        color: var(--hub-blue); 
-        transition: transform 0.3s ease;
+        color: var(--hub-blue);
+        transition: transform 0.2s, background 0.3s, color 0.3s;
     }
 
     .text-container:hover {
+        background: var(--hub-blue);
+        color: white; 
         cursor: none;
-        transform: scale(1.1); 
+        transform: scale(1.1);
     }
 `;
