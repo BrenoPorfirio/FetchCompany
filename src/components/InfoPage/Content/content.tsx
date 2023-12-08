16a5b7829ccf3a06c5edf94ed20af3ffa331ebc6
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-07 21:53:29 -0300

    style for containers

--- src/components/InfoPage/Content/content.tsx
+++ src/components/InfoPage/Content/content.tsx
@@ -2,7 +2,8 @@ import { Container } from '../Content/styles/content';
 
 export function Content() {
     const texts =
-        ["Desperte o Poder do Controle: Gerencie Usuários com Eficiência, Empresas com Maestria e Transforme a Gestão para o Próximo Nível!",
+        [
+            "Desperte o Poder do Controle: Gerencie Usuários com Eficiência, Empresas com Maestria e Transforme a Gestão para o Próximo Nível!",
             "Domine o Mundo Empresarial: Usuários Empoderados, Empresas Otimizadas, Gestão Aprimorada. Seu Sucesso Começa Aqui!",
             "Enfrente Desafios com Confiança: Usuários, Empresas, Excelência em Cada Detalhe. Domine o Poder do Gerenciamento e Supere os Obstáculos com Facilidade!"
         ];
--- src/components/InfoPage/Content/styles/content.ts
+++ src/components/InfoPage/Content/styles/content.ts
@@ -1,20 +1,26 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background-color: var(--hub-blue); 
+    background-color: var(--hub-green); 
 
     .content-container {
         display: flex;
+        flex-wrap: wrap;
         justify-content: space-around;
         align-items: center;
-        height: 91.6vh;
+        height: 91.5vh;
+        padding: 20px;
     }
 
     .text-container {
         border-radius: 8px;
+        background-color: lightgray;
         padding: 20px;
-        border: 1px solid #ccc;
         text-align: center;
         width: 30%;
+        margin: 10px; 
+        font-size: 16px;
+        font-weight: bold;
+        color: var(--hub-blue); 
     }
 `;
