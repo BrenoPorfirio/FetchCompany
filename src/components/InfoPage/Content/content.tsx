1491735582f28381da4fd36f0175055d10474e30
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-07 21:41:23 -0300

    texts to content

--- src/components/InfoPage/Content/content.tsx
+++ src/components/InfoPage/Content/content.tsx
@@ -1,22 +1,21 @@
 import { Container } from '../Content/styles/content';
 
 export function Content() {
-    const texts = ["Melhore sua empresa 1", "Melhore sua empresa 2", "Melhore sua empresa 3"];
+    const texts =
+        ["Desperte o Poder do Controle: Gerencie Usuários com Eficiência, Empresas com Maestria e Transforme a Gestão para o Próximo Nível!",
+            "Domine o Mundo Empresarial: Usuários Empoderados, Empresas Otimizadas, Gestão Aprimorada. Seu Sucesso Começa Aqui!",
+            "Enfrente Desafios com Confiança: Usuários, Empresas, Excelência em Cada Detalhe. Domine o Poder do Gerenciamento e Supere os Obstáculos com Facilidade!"
+        ];
 
     return (
         <Container>
             <div className="content-container">
                 {texts.map((text, index) => (
-                    <div key={index} className="text-container" style={{ backgroundColor: getColor(index) }}>
+                    <div key={index} className="text-container" >
                         {text}
                     </div>
                 ))}
             </div>
         </Container>
     );
-}
-
-function getColor(index) {
-    const colors = ["#ffd700", "#87ceeb", "#90ee90"];
-    return colors[index % colors.length];
 }
